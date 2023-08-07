67afd800fb99ef77413d31dd0b3f16b406b2f9d9
Taylor-L-S <104907080+Taylor-L-S@users.noreply.github.com>
2023-08-07 11:52:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -28,8 +28,36 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text = event.message.text)
-    line_bot_api.reply_message(event.replay_token, message)
+
+    emoji = [
+        {
+            "index":0,
+            "productId":"5ac2213e040ab15980c9b447",
+            "emojiId":"035"
+
+        },
+        {
+            "index":19,
+            "productId":"5ac2213e040ab15980c9b447",
+            "emojiId":"035"
+
+        }
+    ]
+
+    text_message = TextSendMessage(text='''$ stock block_oil $
+您好! 歡迎成為 stock block_oil的好友!
+我是您的股市小幫手
+
+這裡有股票油價等生活經濟資訊唷!
+請直接點選下方連結選單''', emojis = emoji)
+    sticker_message = StickerMessage(
+        package_id = '8522',
+        sticker_id='16581271'
+    )
+
+    line_bot_api.reply_message(
+        event.replay_token, 
+        [text_message, sticker_message])
 
 if __name__ == "__main__":
     app.run()
