5a2982e71234dcf248b8c658a70e0d7802cafb59
Taylor-L-S <104907080+Taylor-L-S@users.noreply.github.com>
2023-08-07 11:02:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ handler = WebhookHandler('880545b3fef970a1460a69447feee723')
 @app.route("/callback", methods = ['POST'])
 def callback():
     ##
-    signature = request.headers['X=Line-Signature']
+    signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
